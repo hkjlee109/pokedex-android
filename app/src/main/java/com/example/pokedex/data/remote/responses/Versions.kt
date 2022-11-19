f50f89878071886ae8907c44bff49c0b92deac5b
harry <hkjlee109@gmail.com>
2022-11-19 13:03:08 +0100

    Setup retrofit

---
+++ app/src/main/java/com/example/pokedex/data/remote/responses/Versions.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex.data.remote.responses
+
+data class Versions(
+    val generationI: GenerationI,
+    val generationIi: GenerationIi,
+    val generationIii: GenerationIii,
+    val generationIV: GenerationIv,
+    val generationV: GenerationV,
+    val generationVi: GenerationVi,
+    val generationVii: GenerationVii,
+    val generationViii: GenerationViii
+)
